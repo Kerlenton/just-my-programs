5fe32b69bd2c3f4ff48e91dfa16cc6df4561df8d
Semyon <51188729+Smekbody@users.noreply.github.com>
2019-07-11 10:31:00 +0400

    Update number_system.c

--- number_system.c
+++ number_system.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#define ENDLESS 1
 
 void bin(char num)
 {
+
 	printf("%d",   (num & 0x80) ? 1 : 0); // 0x80 = 128 = 10000000
 	printf("%d",   (num & 0x40) ? 1 : 0); // 0x40 =  64 = 01000000
 	printf("%d",   (num & 0x20) ? 1 : 0); // 0x20 =  32 = 00100000
@@ -9,4 +12,21 @@ void bin(char num)
 	printf("%d",   (num & 0x04) ? 1 : 0); // 0x04 =   4 = 00000100
 	printf("%d",   (num & 0x02) ? 1 : 0); // 0x02 =   2 = 00000010
 	printf("%d\n", (num & 0x01) ? 1 : 0); // 0x01 =   1 = 00100001
+}
+
+main()
+{
+	int number;
+
+	while (ENDLESS) {
+		scanf_s("%d", &number);
+
+		if (number >= 0 && number <= 255) // check
+			bin(number);
+
+		else
+			printf("Enter a number that is no more than 128 bits or equal to 128 bits\n");
+	}
+
+	return 0;
 }
