f927f0e48f86ef77e8f5432aab24aa9320b62b86
Semyon <51188729+Smekbody@users.noreply.github.com>
2019-07-12 10:01:05 +0400

    Update number_system.c

--- number_system.c
+++ number_system.c
@@ -25,7 +25,7 @@ main()
 			bin(number);
 
 		else
-			printf("Enter a number that is no more than 8 bytes or equal to 8 bytes\n");
+			printf("Enter a number that is no more than 8 bytes or equal to 1 byte\n");
 	}
 
 	return 0;
