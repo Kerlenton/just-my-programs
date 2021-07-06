d85a7388031710100566fbc3cab1f8e18754b46a
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2021-07-06 17:02:11 +0400

    Update main.cpp

--- BN/main.cpp
+++ BN/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    BigNumber x(20);
-    BigNumber y(5);
-    auto z = x * y;
+    BigNumber x(175);
+    BigNumber y(6);
+    auto z = x / y;
     
     for (auto i = z.data_.rbegin(); i != z.data_.rend(); i++)
         cout << *i  << ' ';
